c42464ce6c3440dcf857bf35a956bac5972eb6bd
Miguel Benedit <miguell.beneditt@gmail.com>
2020-09-22 15:54:18 -0300

    Add BaseApiController Add Quest Entity

--- MiguelBeneditProgramación3-WebAppi/V1/Controllers/UserController.cs
+++ MiguelBeneditProgramación3-WebAppi/V1/Controllers/UserController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MiguelBeneditProgramación3_Core.Entities;
+using MiguelBeneditProgramación3_Core.Interfaces.Services;
 
 namespace MiguelBeneditProgramación3_WebApplication.V1.Controllers
 {
-    public class UserController : BaseController
+    public class UserController : BaseApiController<User>
     {
+        public UserController(IBaseService baseService) : base(baseService)
+        {
+        }
     }
 }
