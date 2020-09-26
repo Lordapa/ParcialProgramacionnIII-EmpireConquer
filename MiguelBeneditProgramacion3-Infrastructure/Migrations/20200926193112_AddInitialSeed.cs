1cc875e2d6f27244e47677e18d21d200dba4fc61
Miguel Benedit <miguell.beneditt@gmail.com>
2020-09-26 16:34:20 -0300

    Add InitialSeed

---
+++ MiguelBeneditProgramacion3-Infrastructure/Migrations/20200926193112_AddInitialSeed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace MiguelBeneditProgramacion3_Infrastructure.Migrations
+{
+    public partial class AddInitialSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(File.ReadAllText("../MiguelBeneditProgramacion3-Infrastructure/Data/Seed/Map.sql"));
+            migrationBuilder.Sql(File.ReadAllText("../MiguelBeneditProgramacion3-Infrastructure/Data/Seed/Game.sql"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
